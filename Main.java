2f90b6fefb8c57d37c37c21251dbee287c068f5f
NikitaZemnitskiy <nik.zemnitskiy@gmail.com>
2019-10-18 22:01:00 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,7 @@
 
 public class Main {
 	public static void main(String[] args)  {
-		String H,e,l,o,W,r,d;
-		H = "H";
-		e = "e";
-		l = "l";
-		o = "o";
-		W = "W";
-		r = "r";
-		d = "d";
-		H = H.concat(e);
-		H = H.concat(l);
-		H = H.concat(l);
-		H = H.concat(o);
-		W = W.concat(o);
-		W = W.concat(r);
-		W = W.concat(l);
-		W = W.concat(d);
-		System.out.print(H +" "+ W);
+		for(int i = 0; i<10;i++);
+		System.out.print(""Hellow world");
 	}
 }
